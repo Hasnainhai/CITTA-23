38f7faafa14a40929a1cdc894ca9c577fea9f727
ba138 <basitalyshah51214@gmail.com>
2023-10-15 01:17:42 +0500

    changes to add shawdo to the button

--- lib/view/onBordingScreens/onbording_screen1.dart
+++ lib/view/onBordingScreens/onbording_screen1.dart
@@ -11,70 +11,72 @@ class OnBordingScreen1 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: Padding(
-        padding: const EdgeInsets.only(
-          left: 20,
-          right: 20,
-        ),
-        child: Column(
-          children: [
-            const VerticalSpeacing(80),
-            Container(
-              height: 350,
-              width: MediaQuery.of(context).size.width,
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage("images/onboarding1.png"),
-                ),
-              ),
-            ),
-            const VerticalSpeacing(60),
-            const Text(
-              "Browse all the category",
-              style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.w400,
-                  color: AppColor.fontColor),
-            ),
-            const VerticalSpeacing(16),
-            const Text(
-              "In aliquip aute exercitation ut et nisi ut mollit. Deserunt dolor elit pariatur aute .",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 16,
-              ),
-            ),
-            const VerticalSpeacing(66),
-            CircularPercentIndicator(
-              radius: 56,
-              percent: 0.3,
-              progressColor: AppColor.primaryColor,
-              backgroundColor: AppColor.inActiveColor,
-              center: GestureDetector(
-                onTap: () {},
-                child: Container(
-                  height: 80,
-                  width: 80,
-                  decoration: const BoxDecoration(
-                    shape: BoxShape.circle,
-                    boxShadow: [
-                      BoxShadow(
-                        color: Color(0xffFE0180),
-                        blurRadius: 11,
-                        offset: Offset(0, 2),
-                      ),
-                    ],
-                  ),
-                  child: const Icon(
-                    Icons.arrow_forward,
-                    color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: 20,
+            right: 20,
+          ),
+          child: Column(
+            children: [
+              const VerticalSpeacing(80),
+              Container(
+                height: 350,
+                width: MediaQuery.of(context).size.width,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("images/onboarding1.png"),
                   ),
                 ),
               ),
-            ),
-          ],
+              const VerticalSpeacing(60),
+              const Text(
+                "Browse all the category",
+                style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w400,
+                    color: AppColor.fontColor),
+              ),
+              const VerticalSpeacing(16),
+              const Text(
+                "In aliquip aute exercitation ut et nisi ut mollit. Deserunt dolor elit pariatur aute .",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+              const VerticalSpeacing(66),
+              CircularPercentIndicator(
+                radius: 45,
+                percent: 0.3,
+                progressColor: AppColor.primaryColor,
+                backgroundColor: AppColor.inActiveColor,
+                center: GestureDetector(
+                  onTap: () {},
+                  child: Container(
+                    height: 60,
+                    width: 60,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: AppColor.primaryColor,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color(0xFFFE0180),
+                          blurRadius: 6,
+                          offset: Offset(0, 2),
+                        ),
+                      ],
+                    ),
+                    child: const Icon(
+                      Icons.arrow_forward,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
-      )),
+      ),
     );
   }
 }
