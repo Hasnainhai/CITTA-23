7b33fa5c73fbea097a397f5154a7bea7290c044e
ba138 <basitalyshah51214@gmail.com>
2023-10-15 00:47:13 +0500

    changes to add images ,text to onboarding screen 1

--- lib/res/components/colors.dart
+++ lib/res/components/colors.dart
@@ -2,4 +2,5 @@ import 'package:flutter/material.dart';
 
 class AppColor {
   static const Color splashBgColor = Color(0xffFFE6F2);
+  static const Color fontColor = Color(0xff16162E);
 }
--- lib/view/onBordingScreens/onbording_screen1.dart
+++ lib/view/onBordingScreens/onbording_screen1.dart
@@ -1,27 +1,51 @@
+import 'package:citta_23/res/components/colors.dart';
 import 'package:flutter/material.dart';
+
+import '../../res/components/verticalSpacing.dart';
 
 class OnBordingScreen1 extends StatelessWidget {
   const OnBordingScreen1({super.key});
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       body: SafeArea(
-          child: Column(
-        children: [
-          Container(
-            height: 350,
-            width: MediaQuery.of(context).size.width,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage("images/onboarding1.png"),
+          child: Padding(
+        padding: const EdgeInsets.only(
+          left: 20,
+          right: 20,
+        ),
+        child: Column(
+          children: [
+            const VerticalSpeacing(80),
+            Container(
+              height: 350,
+              width: MediaQuery.of(context).size.width,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("images/onboarding1.png"),
+                ),
               ),
             ),
-          )
-        ],
+            const VerticalSpeacing(60),
+            const Text(
+              "Browse all the category",
+              style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w400,
+                  color: AppColor.fontColor),
+            ),
+            const VerticalSpeacing(16),
+            const Text(
+              "In aliquip aute exercitation ut et nisi ut mollit. Deserunt dolor elit pariatur aute .",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            )
+          ],
+        ),
       )),
     );
-
   }
 }
