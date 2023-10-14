e84f65d336fc3eb1a15aefa01aad043f0495dcea
ba138 <basitalyshah51214@gmail.com>
2023-10-15 00:07:36 +0500

    adding onboarding screen

---
+++ lib/view/onBordingScreens/onbording_screen1.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class OnBordingScreen1 extends StatelessWidget {
+  const OnBordingScreen1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
