f0a9caa7fd02a63600ad0552978243108a1f0023
Hasnain Haider <hasnainhaiderbs2099@gmail.com>
2023-10-15 16:25:33 +0500

    woking on components button component

--- lib/res/components/colors.dart
+++ lib/res/components/colors.dart
@@ -4,6 +4,8 @@ class AppColor {
   static const Color splashBgColor = Color(0xffFFE6F2);
   static const Color fontColor = Color(0xff16162E);
   static const Color primaryColor = Color(0xffCB0166);
-
   static const Color inActiveColor = Color(0xffCFD8DC);
+  static const Color buttonBgColor = Color(0xffCB0166);
+  static const Color buttonTxColor = Color(0xffFFFFFF);
+  static const Color whiteColor=  Color(0xffFFFFFF);
 }
