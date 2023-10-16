ff5e52735ba8c0e3edd1878398388f01c5190cc9
Hasnain Haider <hasnainhaiderbs2099@gmail.com>
2023-10-16 23:15:18 +0500

    Woking on Home Screen Cart

--- lib/view/HomeScreen/homeScreen.dart
+++ lib/view/HomeScreen/homeScreen.dart
@@ -1,12 +1,16 @@
 import 'package:citta_23/res/components/colors.dart';
+import 'package:citta_23/res/components/widgets/verticalSpacing.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+
+import 'widgets/homeCard.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    bool isTrue = true;
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
@@ -60,9 +64,10 @@ class HomeScreen extends StatelessWidget {
                             TextSpan(
                               text: 'Chhatak,Syhlet',
                               style: TextStyle(
-                                  color: AppColor.grayColor,
-                                  fontWeight: FontWeight.w200,
-                                  fontSize: 12.0),
+                                color: AppColor.grayColor,
+                                fontWeight: FontWeight.w200,
+                                fontSize: 12.0,
+                              ),
                             ),
                           ],
                         ),
@@ -100,18 +105,139 @@ class HomeScreen extends StatelessWidget {
           ],
         ),
         body: Padding(
-          padding: const EdgeInsets.only(left: 10.0, right: 10.0, top: 10.0),
-          child: Column(
+          padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+          child: ListView(
             children: [
-              Container(
-                height: 180.0,
-                width: double.infinity,
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('images/bannerImg.png'),
-                    fit: BoxFit.contain,
+              Column(
+                children: [
+                  Container(
+                    height: 180.0,
+                    width: double.infinity,
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('images/bannerImg.png'),
+                        fit: BoxFit.contain,
+                      ),
+                    ),
                   ),
-                ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Popular Pack",
+                        style: GoogleFonts.getFont(
+                          "Gothic A1",
+                          textStyle: const TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: AppColor.fontColor),
+                        ),
+                      ),
+                      Text(
+                        "View All",
+                        style: GoogleFonts.getFont(
+                          "Gothic A1",
+                          textStyle: const TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w600,
+                              color: AppColor.buttonBgColor),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const VerticalSpeacing(16.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      HomeCard(
+                        name: 'Bundle Pack',
+                        categories: 'Onion,Oil,Salt...',
+                        price: '\$35 ',
+                        dPrice: '\$50.32',
+                        borderColor: AppColor.buttonBgColor,
+                        fillColor: AppColor.appBarButtonColor,
+                        cartBorder: isTrue
+                            ? AppColor.appBarButtonColor
+                            : AppColor.buttonBgColor,
+                        img: 'images/fruit1.png',
+                        iconColor: AppColor.buttonBgColor,
+                      ),
+                      HomeCard(
+                        name: 'Fruit Pack',
+                        categories: 'Apple,banana...',
+                        price: '\$50 ',
+                        dPrice: '\$70.32',
+                        borderColor: AppColor.buttonBgColor,
+                        fillColor: isTrue
+                            ? AppColor.buttonBgColor
+                            : AppColor.appBarButtonColor,
+                        cartBorder: AppColor.buttonBgColor,
+                        img: 'images/fruit2.png',
+                        iconColor:
+                            isTrue ? AppColor.whiteColor : AppColor.buttonBgColor,
+                      ),
+                    ],
+                  ),
+                  const VerticalSpeacing(16.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Our New Item",
+                        style: GoogleFonts.getFont(
+                          "Gothic A1",
+                          textStyle: const TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: AppColor.fontColor),
+                        ),
+                      ),
+                      Text(
+                        "View All",
+                        style: GoogleFonts.getFont(
+                          "Gothic A1",
+                          textStyle: const TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w600,
+                              color: AppColor.buttonBgColor),
+                        ),
+                      ),
+                    ],
+                  ),
+                   Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      HomeCard(
+                        name: 'Girl Guide',
+                        categories: '1000 GM',
+                        price: '\$10 ',
+                        dPrice: '\$15.99',
+                        borderColor: AppColor.buttonBgColor,
+                        fillColor: isTrue
+                            ? AppColor.buttonBgColor
+                            : AppColor.appBarButtonColor,
+                        cartBorder: AppColor.buttonBgColor,
+                        img: 'images/fruit3.png',
+                        iconColor:
+                            isTrue ? AppColor.whiteColor : AppColor.buttonBgColor,
+                      ),
+                      HomeCard(
+                        name: 'Tomato',
+                        categories: '1000 GM',
+                        price: '\$5 ',
+                        dPrice: '\$9.99',
+                        borderColor: AppColor.buttonBgColor,
+                        fillColor: isTrue
+                            ? AppColor.buttonBgColor
+                            : AppColor.appBarButtonColor,
+                        cartBorder: AppColor.buttonBgColor,
+                        img: 'images/fruit4.png',
+                        iconColor:
+                            isTrue ? AppColor.whiteColor : AppColor.buttonBgColor,
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ],
           ),
---
+++ lib/view/HomeScreen/widgets/homeCard.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../../../res/components/colors.dart';
+import '../../../res/components/widgets/verticalSpacing.dart';
+
+class HomeCard extends StatelessWidget {
+  const HomeCard(
+      {super.key,
+      required this.name,
+      required this.categories,
+      required this.price,
+      required this.dPrice,
+      required this.borderColor,
+      required this.fillColor,
+      required this.cartBorder,
+      required this.img,
+      required this.iconColor});
+  final String img;
+  final String name;
+  final String categories;
+  final String price;
+  final String dPrice;
+  final Color borderColor;
+  final Color fillColor;
+  final Color cartBorder;
+  final Color iconColor;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Container(
+          height: 210.0,
+          width: MediaQuery.of(context).size.width * 0.43,
+          decoration: BoxDecoration(
+            color: AppColor.appBarButtonColor,
+            border: Border.all(width: 1, color: cartBorder),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.only(left: 15.0, right: 15.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const VerticalSpeacing(20.0),
+                Center(
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: 100,
+                    width: 100,
+                    child: Center(
+                      child: Image.asset(
+                        img,
+                      ),
+                    ),
+                  ),
+                ),
+                const VerticalSpeacing(6.0),
+                Text.rich(
+                  TextSpan(
+                    text: '$name \n',
+                    style: GoogleFonts.getFont(
+                      "Gothic A1",
+                      textStyle: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w800,
+                        color: AppColor.fontColor,
+                      ),
+                    ),
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: categories,
+                        style: const TextStyle(
+                            color: AppColor.grayColor,
+                            // fontWeight: FontWeight.w100,
+                            fontSize: 14.0),
+                      ),
+                    ],
+                  ),
+                ),
+                const VerticalSpeacing(10.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text.rich(
+                      TextSpan(
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: price,
+                            style: GoogleFonts.getFont(
+                              "Gothic A1",
+                              textStyle: const TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w800,
+                                color: AppColor.fontColor,
+                              ),
+                            ),
+                          ),
+                          TextSpan(
+                            text: dPrice,
+                            style: const TextStyle(
+                              color: Colors.grey,
+                              decoration: TextDecoration.lineThrough,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      height: 28.0,
+                      width: 28.0,
+                      decoration: BoxDecoration(
+                        color: fillColor,
+                        border: Border.all(
+                          width: 1.0,
+                          color: borderColor,
+                        ),
+                      ),
+                      child: Center(
+                        child: Icon(Icons.add, color: iconColor),
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
