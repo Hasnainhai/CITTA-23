a8ad4a5ae64cd8bb6d0ae2b8eab0c2d758601601
ba138 <basitalyshah51214@gmail.com>
2023-10-18 15:49:55 +0500

    details screen done

--- lib/view/HomeScreen/product_detail_screen.dart
+++ lib/view/HomeScreen/product_detail_screen.dart
@@ -1,4 +1,7 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:citta_23/res/components/widgets/verticalSpacing.dart';
+import 'package:citta_23/view/HomeScreen/widgets/increase_container.dart';
+import 'package:citta_23/view/HomeScreen/widgets/slider_dots.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -13,6 +16,18 @@ class ProductDetailScreen extends StatefulWidget {
 
 class _ProductDetailScreenState extends State<ProductDetailScreen> {
   bool like = false;
+  int currentIndex = 0;
+  final List<String> imgList = [
+    "images/fruit1.png",
+    "images/fruit2.png",
+    "images/purepng.png",
+  ];
+  List<Color> colors = [
+    AppColor.primaryColor,
+    AppColor.primaryColor,
+    AppColor.primaryColor
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,55 +62,283 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
             right: 20,
             top: 26,
           ),
-          child: Column(
-            children: [
-              Container(
-                height: 320,
-                width: 400,
-                decoration: const BoxDecoration(
-                  color: Color(0xffEEEEEE),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.only(
-                    left: 14,
-                    right: 14,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  height: 320,
+                  width: 400,
+                  decoration: const BoxDecoration(
+                    color: Color(0xffEEEEEE),
                   ),
-                  child: Column(
-                    children: [
-                      const VerticalSpeacing(10),
-                      Row(
-                        children: [
-                          InkWell(
-                            onTap: () {
-                              setState(() {
-                                like = !like; // Toggle the value of like
-                              });
-                            },
-                            child: Container(
-                              height: 48,
-                              width: 48,
-                              color: Colors.white,
-                              child: like
-                                  ? const Icon(
-                                      Icons.favorite,
-                                      color: AppColor.primaryColor,
-                                    )
-                                  : const Icon(Icons.favorite_border_rounded),
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                      left: 14,
+                      right: 14,
+                    ),
+                    child: Column(
+                      children: [
+                        const VerticalSpeacing(10),
+                        Row(
+                          children: [
+                            InkWell(
+                              onTap: () {
+                                setState(() {
+                                  like = !like; // Toggle the value of like
+                                });
+                              },
+                              child: Container(
+                                height: 48,
+                                width: 48,
+                                color: Colors.white,
+                                child: like
+                                    ? const Icon(
+                                        Icons.favorite,
+                                        color: AppColor.primaryColor,
+                                      )
+                                    : const Icon(Icons.favorite_border_rounded),
+                              ),
+                            ),
+                          ],
+                        ),
+                        CarouselSlider(
+                          items: imgList
+                              .map((items) => Container(
+                                    child: Center(
+                                      child: Image.asset(
+                                        items,
+                                        height: 200,
+                                      ),
+                                    ),
+                                  ))
+                              .toList(),
+                          options: CarouselOptions(
+                              enlargeCenterPage: true,
+                              onPageChanged: (index, reason) {
+                                setState(() {
+                                  currentIndex = index % colors.length;
+                                });
+                              }),
+                        ),
+                        const VerticalSpeacing(40),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              width: 18,
+                              height: 5,
+                              margin: const EdgeInsets.symmetric(horizontal: 3),
+                              decoration: BoxDecoration(
+                                color:
+                                    currentIndex == 0 ? colors[0] : Colors.grey,
+                              ),
+                            ),
+                            Container(
+                              width: 10,
+                              height: 5,
+                              margin: const EdgeInsets.symmetric(horizontal: 3),
+                              decoration: BoxDecoration(
+                                color:
+                                    currentIndex == 1 ? colors[1] : Colors.grey,
+                              ),
+                            ),
+                            Container(
+                              width: 10,
+                              height: 5,
+                              margin: const EdgeInsets.symmetric(horizontal: 3),
+                              decoration: BoxDecoration(
+                                color:
+                                    currentIndex == 2 ? colors[2] : Colors.grey,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const VerticalSpeacing(30),
+                Text(
+                  "Cauliflower Bangladeshi",
+                  style: GoogleFonts.getFont(
+                    "Gothic A1",
+                    textStyle: const TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: AppColor.fontColor,
+                    ),
+                  ),
+                ),
+                const VerticalSpeacing(16),
+                Text(
+                  "Weight: 5kg",
+                  style: GoogleFonts.getFont(
+                    "Gothic A1",
+                    textStyle: const TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                      color: AppColor.fontColor,
+                    ),
+                  ),
+                ),
+                const VerticalSpeacing(
+                  28,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          "\$30",
+                          style: GoogleFonts.getFont(
+                            "Gothic A1",
+                            textStyle: const TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: AppColor.fontColor,
+                              decoration: TextDecoration.lineThrough,
                             ),
                           ),
-                        ],
-                      ),
-                      InkWell(
-                        child: Image.asset(
-                          "images/purepng.png",
-                          height: 200,
                         ),
-                      )
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          "\$20",
+                          style: GoogleFonts.getFont(
+                            "Gothic A1",
+                            textStyle: const TextStyle(
+                              fontSize: 28,
+                              fontWeight: FontWeight.bold,
+                              color: AppColor.primaryColor,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    IncreaseContainer()
+                  ],
+                ),
+                const VerticalSpeacing(
+                  20,
+                ),
+                Text(
+                  "Product Details",
+                  style: GoogleFonts.getFont(
+                    "Gothic A1",
+                    textStyle: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                      color: AppColor.fontColor,
+                    ),
+                  ),
+                ),
+                const VerticalSpeacing(
+                  8,
+                ),
+                Text(
+                  "Duis aute veniam veniam qui aliquip irure duis sint magna occaecat dolore nisi culpa do. Est nisi incididunt aliquip  commodo aliqua tempor.",
+                  style: GoogleFonts.getFont(
+                    "Gothic A1",
+                    textStyle: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                      color: AppColor.grayColor,
+                    ),
+                  ),
+                ),
+                const Divider(
+                  thickness: 2,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Review",
+                      style: GoogleFonts.getFont(
+                        "Gothic A1",
+                        textStyle: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: AppColor.fontColor,
+                        ),
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        const Icon(
+                          Icons.star,
+                          color: Colors.amber,
+                        ),
+                        const Icon(
+                          Icons.star,
+                          color: Colors.amber,
+                        ),
+                        const Icon(
+                          Icons.star,
+                          color: Colors.amber,
+                        ),
+                        const Icon(
+                          Icons.star,
+                          color: Colors.amber,
+                        ),
+                        const Icon(
+                          Icons.star,
+                          color: Colors.amber,
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.arrow_forward_ios_outlined,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+                const Divider(
+                  thickness: 2,
+                ),
+                const VerticalSpeacing(
+                  28,
+                ),
+                Container(
+                  height: 50,
+                  width: MediaQuery.of(context).size.width,
+                  color: AppColor.primaryColor,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        height: 50,
+                        width: 60,
+                        color: Colors.white,
+                        child: const Icon(Icons.shopping_bag_outlined),
+                      ),
+                      const SizedBox(
+                        width: 30,
+                      ),
+                      Text(
+                        "Buy Now",
+                        style: GoogleFonts.getFont(
+                          "Gothic A1",
+                          textStyle: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
                     ],
                   ),
                 ),
-              ),
-            ],
+                const VerticalSpeacing(
+                  40,
+                ),
+              ],
+            ),
           ),
         ),
       ),
