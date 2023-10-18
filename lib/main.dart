8d7c56c937a4a1e83fcc50419b2218ad0dbeea8a
Hasnain Haider <hasnainhaiderbs2099@gmail.com>
2023-10-18 23:52:53 +0500

    working on CardPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:citta_23/view/HomeScreen/DashBoard/tapBar.dart';
 import 'package:flutter/material.dart';
 import 'routes/routes.dart';
 import 'routes/routes_name.dart';
+import 'view/HomeScreen/homeScreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,8 +14,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: RoutesName.homeScreen,
+      // initialRoute: RoutesName.homeScreen,
       onGenerateRoute: Routes.generateRoute,
+      home: Home(),
     );
   }
 }
