df060dfe0ccea3222bfd9746e7e00253aa9a9f69
Hasnain Haider <hasnainhaiderbs2099@gmail.com>
2023-10-21 16:27:51 +0500

    creating address checkout widget in CheckOutScreen

--- lib/view/Checkout/check_out.dart
+++ lib/view/Checkout/check_out.dart
@@ -2,6 +2,7 @@ import 'package:citta_23/res/components/widgets/verticalSpacing.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import '../../res/components/colors.dart';
+import 'widgets/myCheckout.dart';
 
 class CheckOutScreen extends StatefulWidget {
   const CheckOutScreen({super.key});
@@ -43,121 +44,109 @@ class _CheckOutScreenState extends State<CheckOutScreen> {
         ),
       ),
       body: SafeArea(
-          child: Padding(
-        padding: const EdgeInsets.only(left: 16.0, right: 16.0),
-        child: Column(
-          children: [
-            const VerticalSpeacing(24.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Select delivery address',
-                  style: GoogleFonts.getFont(
-                    "Gothic A1",
-                    textStyle: const TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w400,
-                      color: AppColor.blackColor,
-                    ),
-                  ),
-                ),
-                TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    'Add New',
+        child: Padding(
+          padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+          child: Column(
+            children: [
+              const VerticalSpeacing(24.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Select delivery address',
                     style: GoogleFonts.getFont(
                       "Gothic A1",
                       textStyle: const TextStyle(
-                        fontSize: 14,
+                        fontSize: 16,
                         fontWeight: FontWeight.w400,
-                        color: AppColor.primaryColor,
+                        color: AppColor.blackColor,
                       ),
                     ),
                   ),
-                )
-              ],
-            ),
-            const VerticalSpeacing(15.0),
-            Container(
-              height: 92,
-              width: MediaQuery.of(context).size.width,
-              color: AppColor.logoBgColor,
-              child: Padding(
-                padding: const EdgeInsets.only(top: 15.0, left: 15.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    MyCheckBox(),
-                    // Checkbox(
-                    //   materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                    //   focusColor: AppColor.primaryColor,
-                    //   activeColor: AppColor.primaryColor,
-                    //   checkColor: AppColor.whiteColor,
-                    //   overlayColor:
-                    //       MaterialStateProperty.all(AppColor.primaryColor),
-                    //   value: isChecked,
-                    //   onChanged: onChanged,
-                    // ),
-                    const SizedBox(width: 20.0),
-                    Text(
-                      'Home Address',
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Add New',
                       style: GoogleFonts.getFont(
                         "Gothic A1",
                         textStyle: const TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
                           color: AppColor.primaryColor,
                         ),
                       ),
                     ),
-                  ],
-                ),
+                  )
+                ],
               ),
-            ),
-          ],
+              // AddressCheckOutWidget(),
+            ],
+          ),
         ),
-      )),
+      ),
     );
   }
 }
 
-class MyCheckBox extends StatefulWidget {
-  @override
-  _MyCheckBoxState createState() => _MyCheckBoxState();
-}
-
-class _MyCheckBoxState extends State<MyCheckBox> {
-  bool isChecked = false;
-
-  void onChanged() {
-    setState(() {
-      isChecked = !isChecked;
-    });
-  }
-
+class AddressCheckOutWidget extends StatelessWidget {
+  const AddressCheckOutWidget({
+    super.key, required this.bgColor, required this.borderColor, required this.titleColor, required this.title, required this.phNo, required this.address,
+  });
+  final Color bgColor;
+  final Color borderColor;
+  final Color titleColor;
+  final String title;
+  final String phNo;
+  final String address;
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: onChanged,
-      child: Container(
-        width: 18,
-        height: 18,
-        decoration: BoxDecoration(
-          border: Border.all(
-            color: isChecked ? AppColor.logoBgColor : AppColor.primaryColor,
-          ),
-        ),
-        child: isChecked
-            ? Center(
-                child: Container(
-                  width: 9.0,
-                  height: 9.0,
-                  color: isChecked ? AppColor.primaryColor : Colors.transparent,
+    return Container(
+      height: 92,
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+          color: AppColor.logoBgColor,
+          border: Border.all(width: 2, color: AppColor.primaryColor)),
+      child: Padding(
+        padding: const EdgeInsets.only(top: 15.0, left: 15.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            MyCheckBox(),
+            const SizedBox(width: 15.0),
+            Text.rich(
+              TextSpan(
+                text: 'Home Address\n ',
+                style: GoogleFonts.getFont(
+                  "Gothic A1",
+                  textStyle: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: AppColor.primaryColor,
+                  ),
                 ),
-              )
-            : null,
+                children: const <TextSpan>[
+                  TextSpan(
+                    text: '(309) 071-9396-939\n',
+                    style: TextStyle(
+                      color: AppColor.grayColor,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 14.0,
+                    ),
+                  ),
+                  TextSpan(
+                    text: '1749 Custom Road, Chhatak',
+                    style: TextStyle(
+                      color: AppColor.grayColor,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 14.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
