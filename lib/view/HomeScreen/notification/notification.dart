ae93aaa6987416b72c0075288e892f6dad0e732b
ba138 <basitalyshah51214@gmail.com>
2023-10-22 03:19:21 +0500

    notification screen done

--- lib/view/HomeScreen/notification/notification.dart
+++ lib/view/HomeScreen/notification/notification.dart
@@ -1,4 +1,9 @@
+import 'package:citta_23/res/components/widgets/verticalSpacing.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../../../res/components/colors.dart';
+import '../../../res/components/widgets/togglw_widget.dart';
 
 class NotificationScreen extends StatelessWidget {
   const NotificationScreen({super.key});
@@ -6,6 +11,30 @@ class NotificationScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+        leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(
+              Icons.arrow_back,
+              color: AppColor.fontColor,
+            )),
+        title: Text(
+          "Notification",
+          style: GoogleFonts.getFont(
+            "Gothic A1",
+            textStyle: const TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w400,
+              color: AppColor.fontColor,
+            ),
+          ),
+        ),
+      ),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.only(
@@ -21,7 +50,24 @@ class NotificationScreen extends StatelessWidget {
                     right: 16,
                   ),
                   child: Column(
-                    children: [],
+                    children: [
+                      const VerticalSpeacing(30),
+                      const ToggleWidget(
+                        title: "App Notification",
+                      ),
+                      const Divider(),
+                      const VerticalSpeacing(20),
+                      const ToggleWidget(
+                        title: "Phone Number Notification",
+                      ),
+                      const Divider(),
+                      const VerticalSpeacing(20),
+                      const ToggleWidget(
+                        title: "Offer Notification",
+                      ),
+                      const Divider(),
+                      VerticalSpeacing(MediaQuery.of(context).size.height / 2),
+                    ],
                   ),
                 ),
               )
