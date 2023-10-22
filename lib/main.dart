9f6f02f3f5e4a718df9c5c01905f16b51787d1fb
Hasnain Haider <hasnainhaiderbs2099@gmail.com>
2023-10-23 00:05:40 +0500

    working on payMent Screen

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: RoutesName.ratingscreen,
+      initialRoute: RoutesName.dashboardScreen,
       onGenerateRoute: Routes.generateRoute,
     );
   }
---
+++ lib/view/payments/payment.dart
@@ -0,0 +1,224 @@
+import 'package:citta_23/res/components/widgets/verticalSpacing.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import '../../res/components/colors.dart';
+import 'package:currency_picker/currency_picker.dart';
+
+class Payment extends StatelessWidget {
+  const Payment({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+        leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(
+              Icons.arrow_back,
+              color: AppColor.fontColor,
+            )),
+        title: Text(
+          "Payment",
+          style: GoogleFonts.getFont(
+            "Gothic A1",
+            textStyle: const TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w400,
+              color: AppColor.fontColor,
+            ),
+          ),
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const VerticalSpeacing(30.0),
+          Padding(
+            padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "My Card",
+                  style: GoogleFonts.getFont(
+                    "Gothic A1",
+                    textStyle: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w400,
+                      color: AppColor.fontColor,
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 32.0,
+                  width: 32.0,
+                  color: AppColor.primaryColor,
+                  child: const Center(
+                    child: Icon(
+                      Icons.add,
+                      color: AppColor.whiteColor,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const VerticalSpeacing(20.0),
+          SizedBox(
+            height: 176,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.85,
+                  child: Image.asset('images/masterCart.png'),
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.85,
+                  child: Image.asset('images/visaCard.png'),
+                ),
+              ],
+            ),
+          ),
+          const VerticalSpeacing(46.0),
+          Padding(
+            padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Other Payment Options",
+                  style: GoogleFonts.getFont(
+                    "Gothic A1",
+                    textStyle: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                      color: AppColor.fontColor,
+                    ),
+                  ),
+                ),
+                const VerticalSpeacing(20.0),
+                Container(
+                  height: 85,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      width: 1.0,
+                      color: AppColor.primaryColor,
+                    ),
+                  ),
+                  child: Row(
+                    children: [
+                      const SizedBox(width: 10.0),
+                      Container(
+                        height: 60.0,
+                        width: 60.0,
+                        color: const Color(0xffEEEEEE),
+                        child: Center(
+                          child: SizedBox(
+                            height: 40.0,
+                            width: 40.0,
+                            child: Image.asset(
+                              'images/paypal.png',
+                              fit: BoxFit.contain,
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 25.0),
+                      Text.rich(
+                        TextSpan(
+                          children: <TextSpan>[
+                            TextSpan(
+                              text: 'Paypal   \n',
+                              style: GoogleFonts.getFont(
+                                "Gothic A1",
+                                textStyle: const TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w700,
+                                  color: AppColor.fontColor,
+                                ),
+                              ),
+                            ),
+                            const TextSpan(
+                              text: 'MyPaypal@gmail.com',
+                              style: TextStyle(
+                                color: AppColor.fontColor,
+                                fontSize: 16.0,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const VerticalSpeacing(20.0),
+                Container(
+                  height: 85,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      width: 1.0,
+                      color: const Color(0xffEEEEEE),
+                    ),
+                  ),
+                  child: Row(
+                    children: [
+                      const SizedBox(width: 10.0),
+                      Container(
+                        height: 60.0,
+                        width: 60.0,
+                        color: const Color(0xffEEEEEE),
+                        child: Center(
+                          child: SizedBox(
+                            height: 40.0,
+                            width: 40.0,
+                            child: Image.asset(
+                              'images/cash.png',
+                              fit: BoxFit.contain,
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 25.0),
+                      Text.rich(
+                        TextSpan(
+                          children: <TextSpan>[
+                            TextSpan(
+                              text: 'Cash On Delivery   \n',
+                              style: GoogleFonts.getFont(
+                                "Gothic A1",
+                                textStyle: const TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w700,
+                                  color: AppColor.fontColor,
+                                ),
+                              ),
+                            ),
+                            const TextSpan(
+                              text: 'Pay in Cash',
+                              style: TextStyle(
+                                color: AppColor.fontColor,
+                                fontSize: 16.0,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
