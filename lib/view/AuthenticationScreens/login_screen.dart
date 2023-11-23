e49da5ea63aacdc659f7cc8267e94c7e9baee4f0
Hasnain Haider <hasnainhaiderbs2099@gmail.com>
2023-11-23 21:13:33 +0500

    working on auth

--- lib/view/AuthenticationScreens/login_screen.dart
+++ lib/view/AuthenticationScreens/login_screen.dart
@@ -9,9 +9,7 @@ import 'package:citta_23/routes/routes_name.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-
 import '../../res/components/colors.dart';
-import '../../res/components/widgets/testAuth.dart';
 import '../../res/consts/firebase_const.dart';
 import '../../utils/utils.dart';
 
