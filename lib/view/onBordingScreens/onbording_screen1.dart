016452e3b305f44ce2e3115ec06a25abe42e0adc
Hasnain Haider <hasnainhaiderbs2099@gmail.com>
2023-11-25 01:55:10 +0500

    store user details in firebase

--- lib/view/onBordingScreens/onbording_screen1.dart
+++ lib/view/onBordingScreens/onbording_screen1.dart
@@ -17,49 +17,52 @@ class OnBordingScreen1 extends StatelessWidget {
             left: 20,
             right: 20,
           ),
-          child: Column(
-            children: [
-              const VerticalSpeacing(80),
-              Container(
-                height: 350,
-                width: MediaQuery.of(context).size.width,
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage("images/onboarding1.png"),
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
+              children: [
+                const VerticalSpeacing(80),
+                Container(
+                  height: 350,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("images/onboarding1.png"),
+                    ),
                   ),
                 ),
-              ),
-              const VerticalSpeacing(30),
-              Text(
-                "Browse all the category",
-                style: GoogleFonts.getFont(
-                  "Gothic A1",
-                  textStyle: const TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.w400,
-                      color: AppColor.fontColor),
+                const VerticalSpeacing(30),
+                Text(
+                  "Browse all the category",
+                  style: GoogleFonts.getFont(
+                    "Gothic A1",
+                    textStyle: const TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w400,
+                        color: AppColor.fontColor),
+                  ),
                 ),
-              ),
-              const VerticalSpeacing(16),
-              Text(
-                "In aliquip aute exercitation ut et nisi ut mollit. Deserunt dolor elit pariatur aute .",
-                textAlign: TextAlign.center,
-                style: GoogleFonts.getFont(
-                  "Gothic A1",
-                  textStyle: const TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w400,
-                      color: AppColor.fontColor),
+                const VerticalSpeacing(16),
+                Text(
+                  "In aliquip aute exercitation ut et nisi ut mollit. Deserunt dolor elit pariatur aute .",
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.getFont(
+                    "Gothic A1",
+                    textStyle: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                        color: AppColor.fontColor),
+                  ),
                 ),
-              ),
-              const VerticalSpeacing(46),
-              OnButton(
-                progress: 0.3,
-                onTap: () {
-                  Navigator.pushNamed(context, RoutesName.onboarding2);
-                },
-              ),
-            ],
+                const VerticalSpeacing(46),
+                OnButton(
+                  progress: 0.3,
+                  onTap: () {
+                    Navigator.pushNamed(context, RoutesName.onboarding2);
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
