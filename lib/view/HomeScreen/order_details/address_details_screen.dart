2d35b8b486ca158ae6bea5fa9aa027eae8a5a2fd
Basit ali <basitalyshah51214@gmail.com>
2023-12-20 02:59:38 -0800

    changes to add controller to detail address screen

--- lib/view/HomeScreen/order_details/address_details_screen.dart
+++ lib/view/HomeScreen/order_details/address_details_screen.dart
@@ -16,6 +16,26 @@ class AddressDetailSceen extends StatefulWidget {
 }
 
 class _AddressDetailSceenState extends State<AddressDetailSceen> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController phoneController = TextEditingController();
+  final TextEditingController address1Controller = TextEditingController();
+  final TextEditingController address2Controller = TextEditingController();
+  final TextEditingController cityController = TextEditingController();
+  final TextEditingController stateController = TextEditingController();
+  final TextEditingController zipCodeController = TextEditingController();
+  @override
+  void dispose() {
+    nameController.dispose();
+    phoneController.dispose();
+    address1Controller.dispose();
+    address2Controller.dispose();
+    cityController.dispose();
+    stateController.dispose();
+    zipCodeController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,84 +70,106 @@ class _AddressDetailSceenState extends State<AddressDetailSceen> {
             right: 20,
           ),
           child: SingleChildScrollView(
-            child: Column(
-              children: [
-                const VerticalSpeacing(
-                  20,
-                ),
-                Container(
-                  color: Colors.white,
-                  width: MediaQuery.of(context).size.width,
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 16, right: 16),
-                    child: Column(
-                      children: [
-                        const TextFieldCustom(maxLines: 1, text: "Full Name"),
-                        const TextFieldCustom(
-                            maxLines: 1, text: "Phone Number"),
-                        const TextFieldCustom(
-                            maxLines: 1, text: "Address Link 1"),
-                        const TextFieldCustom(
-                            maxLines: 1, text: "Address Link 2"),
-                        const TextFieldCustom(maxLines: 1, text: "City"),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            SizedBox(
-                              height: 100,
-                              width: MediaQuery.of(context).size.width * 0.35,
-                              child: const TextFieldCustom(
-                                maxLines: 1,
-                                text: 'State',
-                                hintText: '',
-                              ),
-                            ),
-                            SizedBox(
-                              height: 100,
-                              width: MediaQuery.of(context).size.width * 0.35,
-                              child: const TextFieldCustom(
-                                maxLines: 2,
-                                text: 'Zip Code',
-                                hintText: '',
-                              ),
-                            ),
-                          ],
-                        ),
-                        const VerticalSpeacing(20),
-                        Row(
-                          children: [
-                            const MyCheckBox(),
-                            const SizedBox(
-                              width: 10,
-                            ),
-                            Text(
-                              "Make Default Shipping Address",
-                              style: GoogleFonts.getFont(
-                                "Gothic A1",
-                                textStyle: const TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w400,
-                                  color: AppColor.fontColor,
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  const VerticalSpeacing(
+                    20,
+                  ),
+                  Container(
+                    color: Colors.white,
+                    width: MediaQuery.of(context).size.width,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 16, right: 16),
+                      child: Column(
+                        children: [
+                          TextFieldCustom(
+                            maxLines: 1,
+                            text: "Full Name",
+                            controller: nameController,
+                          ),
+                          TextFieldCustom(
+                            maxLines: 1,
+                            text: "Phone Number",
+                            controller: phoneController,
+                          ),
+                          TextFieldCustom(
+                            maxLines: 1,
+                            text: "Address Link 1",
+                            controller: address1Controller,
+                          ),
+                          TextFieldCustom(
+                            maxLines: 1,
+                            text: "Address Link 2",
+                            controller: address2Controller,
+                          ),
+                          TextFieldCustom(
+                            maxLines: 1,
+                            text: "City",
+                            controller: cityController,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              SizedBox(
+                                height: 120,
+                                width: MediaQuery.of(context).size.width * 0.35,
+                                child: TextFieldCustom(
+                                  maxLines: 1,
+                                  text: 'State',
+                                  hintText: '',
+                                  controller: stateController,
                                 ),
                               ),
-                            ),
-                          ],
-                        ),
-                        const VerticalSpeacing(20),
-                        RoundedButton(
-                            title: "Save Address",
-                            onpress: () {
-                              Navigator.pushNamed(
-                                context,
-                                RoutesName.checkOutScreen,
-                              );
-                            }),
-                        const VerticalSpeacing(40),
-                      ],
+                              SizedBox(
+                                height: 120,
+                                width: MediaQuery.of(context).size.width * 0.35,
+                                child: TextFieldCustom(
+                                  maxLines: 2,
+                                  text: 'Zip Code',
+                                  hintText: '',
+                                  controller: zipCodeController,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const VerticalSpeacing(20),
+                          Row(
+                            children: [
+                              const MyCheckBox(),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              Text(
+                                "Make Default Shipping Address",
+                                style: GoogleFonts.getFont(
+                                  "Gothic A1",
+                                  textStyle: const TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w400,
+                                    color: AppColor.fontColor,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          const VerticalSpeacing(20),
+                          RoundedButton(
+                              title: "Save Address",
+                              onpress: () {
+                                Navigator.pushNamed(
+                                  context,
+                                  RoutesName.checkOutScreen,
+                                );
+                              }),
+                          const VerticalSpeacing(40),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
