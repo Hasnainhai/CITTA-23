09a2c1ec8e883722c50ed972587eba48e379c2a4
Basit ali <basitalyshah51214@gmail.com>
2023-12-23 04:40:11 -0800

    changes to add stripe to the app

--- android/app/src/main/kotlin/com/example/citta_23/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/citta_23/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.citta_23
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+
+
+class MainActivity: FlutterFragmentActivity() {
 }
