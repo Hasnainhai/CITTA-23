94bc48068a9ec437872b74f0a1887e9b6b72ea31
Hasnain Haider <hasnainhaiderbs2099@gmail.com>
2024-01-02 01:30:03 +0500

    working on search

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,8 @@ import 'routes/routes_name.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Stripe.publishableKey = "";
-  // "pk_live_51MqJ7aSDFxQSCBeqQ84Nu6z4Tz5S0UL2ExT0tNT0l0HnOuuqHwGGmPGXuaUYpuudJ1H5I4nr4bxeScgIEfk9tIPQ00Lr668Udh";
+  Stripe.publishableKey =
+      "pk_live_51MqJ7aSDFxQ÷SCBeqQ84Nu6z4Tz5S0UL2ExT0tNT0l0HnOuuqHwGGmPGXuaUYpuudJ1H5I4nr4bxeScgIEfk9tIPQ00Lr668Udh";
   Stripe.instance.applySettings();
   await Firebase.initializeApp();
   runApp(
@@ -27,6 +27,7 @@ void main() async {
     ),
   );
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
