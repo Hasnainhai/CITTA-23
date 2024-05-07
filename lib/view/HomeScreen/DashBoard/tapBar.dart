83efa4172d2fa6253dca9fc79baaa7bcd53e4e2f
Hasnain Haider <hasnainhaiderbs2099@gmail.com>
2024-05-07 12:28:44 +0500

    changes in tap bar

--- lib/view/HomeScreen/DashBoard/tapBar.dart
+++ lib/view/HomeScreen/DashBoard/tapBar.dart
@@ -3,7 +3,6 @@
 import 'package:citta_23/res/components/colors.dart';
 import 'package:citta_23/routes/routes_name.dart';
 import 'package:citta_23/view/HomeScreen/homeScreen.dart';
-import 'package:citta_23/view/card/card_screen.dart';
 import 'package:citta_23/view/menu/menu.dart';
 import 'package:citta_23/view/profile/profile_screen.dart';
 import 'package:flutter/material.dart';
@@ -31,7 +30,7 @@ class _DashBoardScreenState extends State<DashBoardScreen>
   @override
   void initState() {
     super.initState();
-    tabController = TabController(length: 5, vsync: this);
+    tabController = TabController(length: 4, vsync: this);
   }
 
   @override
@@ -43,78 +42,73 @@ class _DashBoardScreenState extends State<DashBoardScreen>
         children: const [
           HomeScreen(),
           MenuScreen(),
-          CardScreen(),
           FavouriteList(),
           ProfileScreen(),
         ],
       ),
-      bottomNavigationBar: Stack(
-        children: [
-          BottomNavigationBar(
-            items: const [
-              BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.home,
-                ),
-                label: ('Home'),
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.list_alt_outlined,
-                ),
-                label: ('Menu'),
-              ),
-              BottomNavigationBarItem(
-                  icon: Icon(
-                    Icons.shopping_bag_outlined,
-                  ),
-                  label: ('Cart')),
-              BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.bookmark_outline_rounded,
-                ),
-                label: ('Save'),
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.account_circle,
-                ),
-                label: ('Profile'),
-              ),
-            ],
-            unselectedItemColor: AppColor.grayColor,
-            selectedItemColor: AppColor.buttonBgColor,
-            backgroundColor: Colors.white,
-            type: BottomNavigationBarType.fixed,
-            showUnselectedLabels: true,
-            currentIndex: selectIndex,
-            onTap: onItemClick,
-          ),
-          Positioned(
-            left: MediaQuery.of(context).size.width / 2 - 30,
-            bottom: 0,
-            child: InkWell(
-              onTap: () {
-                Navigator.pushNamed(context, RoutesName.cartScreen);
-              },
-              child: Container(
-                color: AppColor.primaryColor,
-                height: 60,
-                width: 60,
-                child: const Center(
-                  child: ImageIcon(
-                    AssetImage(
-                      "images/card.png",
-                    ),
-                    color: Colors.white,
-                    size: 40,
-                  ),
-                ),
-              ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
             ),
+            label: ('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.list_alt_outlined,
+            ),
+            label: ('Menu'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.bookmark_outline_rounded,
+            ),
+            label: ('Save'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.account_circle,
+            ),
+            label: ('Profile'),
           ),
         ],
+        unselectedItemColor: AppColor.grayColor,
+        selectedItemColor: AppColor.buttonBgColor,
+        backgroundColor: Colors.white,
+        type: BottomNavigationBarType.fixed,
+        showUnselectedLabels: true,
+        currentIndex: selectIndex,
+        onTap: onItemClick,
       ),
+      floatingActionButton: Container(
+        width: 60.0,
+        height: 60.0,
+        decoration: const BoxDecoration(
+          color: AppColor.primaryColor,
+          boxShadow: [
+            BoxShadow(
+              color: Color.fromRGBO(254, 1, 128, 0.2588),
+              blurRadius: 18.0,
+              offset: Offset(0, 12),
+            ),
+          ],
+        ),
+        child: FloatingActionButton(
+          onPressed: () {
+            Navigator.pushNamed(context, RoutesName.cartScreen);
+          },
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Image.asset('images/card.png'),
+            ),
+          ),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
