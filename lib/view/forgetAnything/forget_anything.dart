b3b4b9e56337f1d81ebc5edb610bfc04371546c6
Hasnain Haider <hasnainhaiderbs2099@gmail.com>
2024-06-15 14:44:42 +0500

    working on related products

--- lib/view/forgetAnything/forget_anything.dart
+++ lib/view/forgetAnything/forget_anything.dart
@@ -1,3 +1,5 @@
+import 'package:citta_23/utils/utils.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:citta_23/res/components/widgets/verticalSpacing.dart';
 import 'package:citta_23/view/HomeScreen/widgets/homeCard.dart';
 import 'package:flutter/material.dart';
@@ -7,12 +9,36 @@ import '../Checkout/widgets/card_checkout_screen.dart';
 class ForgetAnything extends StatelessWidget {
   const ForgetAnything({
     super.key,
-    required this.productList,
     required this.subTotal,
+    required this.productList,
   });
 
-  final List<Map<String, dynamic>> productList;
   final String subTotal;
+  final List<Map<String, dynamic>> productList;
+
+  Future<List<Map<String, dynamic>>> fetchRelatedProducts(
+      BuildContext context) async {
+    List<Map<String, dynamic>> relatedProducts = [];
+
+    try {
+      QuerySnapshot fashionSnapshot =
+          await FirebaseFirestore.instance.collection('fashion').get();
+      QuerySnapshot productsSnapshot =
+          await FirebaseFirestore.instance.collection('products').get();
+
+      for (var doc in fashionSnapshot.docs) {
+        relatedProducts.add(doc.data() as Map<String, dynamic>);
+      }
+      for (var doc in productsSnapshot.docs) {
+        relatedProducts.add(doc.data() as Map<String, dynamic>);
+      }
+    } catch (e) {
+      Utils.flushBarErrorMessage(
+          'Error fetching related products: $e', context);
+    }
+
+    return relatedProducts;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -63,30 +89,46 @@ class ForgetAnything extends StatelessWidget {
           ),
         ),
       ),
-      body: GridView.builder(
-        padding: const EdgeInsets.only(left: 16, right: 16),
-        itemCount: productList.length,
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2, // Number of columns
-          crossAxisSpacing: 10.0,
-          mainAxisSpacing: 10.0,
-          childAspectRatio: 4 / 4,
-        ),
-        itemBuilder: (context, index) {
-          final product = productList[index];
-          return HomeCard(
-            name: product['title'],
-            price: '\$${product['salePrice']}',
-            dPrice: '\$${product['salePrice']}',
-            borderColor: AppColor.primaryColor,
-            fillColor: AppColor.bgColor,
-            img: product['imageUrl'],
-            iconColor: AppColor.primaryColor,
-            ontap: () {},
-            addCart: () {},
-            productId: '',
-            sellerId: '',
-            productRating: 0.0,
+      body: FutureBuilder<List<Map<String, dynamic>>>(
+        future: fetchRelatedProducts(context),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return const Center(child: Text('Error fetching related products'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text('No related products found'));
+          }
+
+          final relatedProducts = snapshot.data!;
+          return GridView.builder(
+            padding: const EdgeInsets.only(left: 16,right: 16,bottom: 80),
+            itemCount: relatedProducts.length,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2, // Number of columns
+              crossAxisSpacing: 10.0,
+              mainAxisSpacing: 10.0,
+              childAspectRatio: 4 / 4,
+            ),
+            itemBuilder: (context, index) {
+              final product = relatedProducts[index];
+              return HomeCard(
+                name: product['title'],
+                price: '\$${product['price']}',
+                dPrice: '00', // Adjust as needed
+                borderColor: AppColor.primaryColor,
+                fillColor: AppColor.bgColor,
+                img: product['imageUrl'],
+                iconColor: AppColor.primaryColor,
+                ontap: () {},
+                addCart: () {},
+                productId: product['id'],
+                sellerId: product['sellerId'],
+                productRating: product.containsKey('averageReview')
+                    ? product['averageReview']
+                    : 0.0,
+              );
+            },
           );
         },
       ),
