e0b052d07ee2b41a6504c480ed60476cd9f0af53
Hasnain Haider <hasnainhaiderbs2099@gmail.com>
2024-06-21 16:14:23 +0500

    working on all_releated products

--- lib/view/HomeScreen/all_related_prod.dart
+++ lib/view/HomeScreen/all_related_prod.dart
@@ -1,9 +1,9 @@
-import 'package:citta_23/res/components/loading_manager.dart';
-import 'package:citta_23/view/HomeScreen/product_detail_screen.dart';
+import 'package:citta_23/view/HomeScreen/widgets/homeCard.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-
-import '../../res/components/colors.dart';
+import 'package:citta_23/res/components/loading_manager.dart';
+import 'package:citta_23/res/components/colors.dart';
+import 'package:citta_23/view/HomeScreen/product_detail_screen.dart';
 
 class AllRelatedProd extends StatefulWidget {
   const AllRelatedProd({super.key});
@@ -13,10 +13,8 @@ class AllRelatedProd extends StatefulWidget {
 }
 
 class _AllRelatedProdState extends State<AllRelatedProd> {
-  //get related products
   final List<Map<String, dynamic>> _relatedProducts = [];
   final _firestoreInstance = FirebaseFirestore.instance;
-
   bool _isLoading = false;
 
   fetchRelatedProducts() async {
@@ -26,8 +24,7 @@ class _AllRelatedProdState extends State<AllRelatedProd> {
       });
 
       QuerySnapshot qn = await _firestoreInstance.collection('products').get();
-      debugPrint(
-          'Data fetched from Firestore ${qn.docs}'); // Debugging statement
+      debugPrint('Data fetched from Firestore ${qn.docs}');
 
       setState(() {
         _relatedProducts.clear();
@@ -45,23 +42,25 @@ class _AllRelatedProdState extends State<AllRelatedProd> {
               'detail': data['detail'],
               'weight': data['weight'],
               'category': data['category'],
+              'discount': data.containsKey('discount')
+                  ? data['discount'].toString()
+                  : '0',
+              'averageReview': data.containsKey('averageReview')
+                  ? data['averageReview']
+                  : 0.0,
+              'color': data.containsKey('color') ? data['color'] : [],
+              'size': data.containsKey('size') ? data['size'] : [],
             };
 
-            if (data.containsKey('discount') && data['discount'] != null) {
-              product['discount'] = data['discount'].toString();
-            }
-
-            debugPrint('Fetched product: $product'); // Debugging statement
-
+            debugPrint('Fetched product: $product');
             _relatedProducts.add(product);
           }
         }
         _isLoading = false;
-
-        debugPrint('All Products: $_relatedProducts'); // Debugging statement
+        debugPrint('All Products: $_relatedProducts');
       });
     } catch (e) {
-      debugPrint('Error fetching products: $e'); // Debugging statement
+      debugPrint('Error fetching products: $e');
       setState(() {
         _isLoading = false;
       });
@@ -70,20 +69,13 @@ class _AllRelatedProdState extends State<AllRelatedProd> {
 
   String calculateDiscountedPrice(
       String originalPriceString, String discountPercentageString) {
-    // Convert strings to double
-    debugPrint("this is the discount:$discountPercentageString");
-    debugPrint("this is the total:$originalPriceString");
-
     double originalPrice = double.parse(originalPriceString);
     double discountPercentage = double.parse(discountPercentageString);
 
-    // Calculate discounted price
     double p = originalPrice * (discountPercentage / 100);
     double discountedPrice = originalPrice - p;
 
-    // Return the discounted price as a formatted string
-    return discountedPrice.toStringAsFixed(
-        0); // You can adjust the number of decimal places as needed
+    return discountedPrice.toStringAsFixed(0);
   }
 
   @override
@@ -107,110 +99,70 @@ class _AllRelatedProdState extends State<AllRelatedProd> {
       body: LoadingManager(
         isLoading: _isLoading,
         child: SafeArea(
-          child: ListView.builder(
-            scrollDirection: Axis.vertical,
-            itemCount: _relatedProducts.length,
-            itemBuilder: (context, index) {
-              if (_relatedProducts.isEmpty) {
-                return const Center(
-                  child: Text('Empty related products'),
-                );
-              } else {
-                final categoryRelatedProducts = _relatedProducts[index];
-                return InkWell(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) {
-                          return ProductDetailScreen(
-                            title: categoryRelatedProducts['title'].toString(),
-                            productId: categoryRelatedProducts['id'].toString(),
-                            sellerId:
-                                categoryRelatedProducts['sellerId'].toString(),
-                            imageUrl: categoryRelatedProducts['imageUrl'],
-                            price: categoryRelatedProducts['price'].toString(),
-                            salePrice: categoryRelatedProducts['category'] ==
-                                    "Lightening Deals"
-                                ? calculateDiscountedPrice(
-                                    categoryRelatedProducts['price'],
-                                    categoryRelatedProducts['discount'])
-                                : categoryRelatedProducts['price'].toString(),
-                            weight:
-                                categoryRelatedProducts['weight'].toString(),
-                            detail:
-                                categoryRelatedProducts['detail'].toString(),
-                            disPrice: categoryRelatedProducts['category'] ==
-                                    "Lightening Deals"
-                                ? (int.parse(categoryRelatedProducts[
-                                            'discount']) /
-                                        100 *
-                                        int.parse(
-                                            categoryRelatedProducts['price']))
-                                    .toString()
-                                : '0',
-                          );
-                        },
-                      ),
-                    );
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      width: 206,
-                      color: const Color(0xffEEEEEE),
-                      margin: const EdgeInsets.symmetric(
-                        horizontal: 6.0,
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Container(
-                            height: 63,
-                            width: 84,
-                            color: const Color(0xffC4C4C4),
-                            child: Center(
-                              child: Image.network(
-                                categoryRelatedProducts['imageUrl'],
-                                height: 50,
-                                width: 60,
-                              ),
-                            ),
-                          ),
-                          const SizedBox(width: 8.0),
-                          Expanded(
-                            child: Text.rich(
-                              TextSpan(
-                                text: '${categoryRelatedProducts['title']}\n',
-                                style: const TextStyle(
-                                  fontFamily: 'CenturyGothic',
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.bold,
-                                  color: AppColor.fontColor,
-                                ),
-                                children: [
-                                  TextSpan(
-                                    text:
-                                        '₹${categoryRelatedProducts['price']}',
-                                    style: const TextStyle(
-                                      fontFamily: 'CenturyGothic',
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.w600,
-                                      color: AppColor.buttonBgColor,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
+          child: _relatedProducts.isEmpty
+              ? const Center(child: Text('No Products...'))
+              : Padding(
+                  padding: const EdgeInsets.only(left: 16, right: 16, top: 24),
+                  child: GridView.builder(
+                    physics: const NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: _relatedProducts.length,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 5,
+                      mainAxisSpacing: 16,
                     ),
+                    itemBuilder: (context, index) {
+                      final product = _relatedProducts[index];
+                      return HomeCard(
+                        ontap: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
+                            return ProductDetailScreen(
+                              sellerId: product['sellerId'],
+                              productId: product['id'],
+                              title: product['title'].toString(),
+                              imageUrl: product['imageUrl'],
+                              salePrice: product['price'],
+                              detail: product['detail'].toString(),
+                              price: product['price'],
+                              weight: product['weight'],
+                              disPrice: product['price'],
+                            );
+                          }));
+                        },
+                        sellerId: product['sellerId'],
+                        productId: product['id'],
+                        name: product['title'].toString(),
+                        price: '',
+                        dPrice: "${product['price']}₹",
+                        borderColor: AppColor.buttonBgColor,
+                        fillColor: AppColor.appBarButtonColor,
+                        img: product['imageUrl'],
+                        iconColor: AppColor.buttonBgColor,
+                        addCart: () {
+                          // addToCart(
+                          //   product['imageUrl'],
+                          //   product['title'],
+                          //   product['price'],
+                          //   product['id'],
+                          //   product['sellerId'],
+                          //   product['size'].isNotEmpty
+                          //       ? product['size'][0]
+                          //       : 'N/A',
+                          //   product['color'].isNotEmpty
+                          //       ? product['color'][0]
+                          //       : 'N/A',
+                          //   "N/A",
+                          //   '0',
+                          // );
+                        },
+                        productRating: product['averageReview'],
+                      );
+                    },
                   ),
-                );
-              }
-            },
-          ),
+                ),
         ),
       ),
     );
